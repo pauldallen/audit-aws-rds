fedf991beb750ad86a6f43cbc17a2b78de01c80c
123prove123 <123prove123@gmail.com>
2016-12-21 08:54:53 +0600

    update version

--- services/config.rb
+++ services/config.rb
@@ -79,7 +79,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-rds" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.1.7"
+                   :version => "1.2.5"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
                 "plan name":"PLAN::name",
