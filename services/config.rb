90484a74cbeb806131e7582b24cb72c3712fd01e
123prove123 <123prove123@gmail.com>
2017-01-17 09:25:17 +0600

    update version and fixed bugs for table yaml

--- services/config.rb
+++ services/config.rb
@@ -152,7 +152,7 @@ end
   HTML SEND METHOD
 =end
 coreo_uni_util_notify "advise-rds-json" do
-  action :nothing
+  action :notify
   type 'email'
   allow_empty ${AUDIT_AWS_RDS_ALLOW_EMPTY}
   send_on '${AUDIT_AWS_RDS_SEND_ON}'
