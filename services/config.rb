bb59cbd1424728146ad0aec6861c07a1af1aa1b0
123prove123 <123prove123@gmail.com>
2017-01-18 17:05:19 +0600

    add logic for auditPanel

--- services/config.rb
+++ services/config.rb
@@ -248,7 +248,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-rds" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.5.3"
+                   :version => "1.5.5"
                }
                   ])
   json_input '{ "composite name":"PLAN::stack_name",
